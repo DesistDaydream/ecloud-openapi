9ee86f3bbb6c60c57136f3fe1b0ff34e8e523613
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2023-07-11 17:43:37 +0800

    通过excel文件创建安全组规则逻辑的问题已修复

--- cmd/vpc/security_group_rule_create.go
+++ cmd/vpc/security_group_rule_create.go
@@ -1,6 +1,7 @@
 package vpc
 
 import (
+	"github.com/DesistDaydream/ecloud-openapi/pkg/fileparse"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"gitlab.ecloud.com/ecloud/ecloudsdkvpc/model"
@@ -19,6 +20,14 @@ func ruleCreateCommand() *cobra.Command {
 }
 
 func securityGroupRuleCreate(cmd *cobra.Command, args []string) {
+	// 实例值：
+	// remoteType := "cidr"
+	// protocol := "ANY"
+	// etherType := "IPv4"
+	// description := "测试描述"
+	// direction := "ingress"
+	// remoteIpPrefix := "1.1.1.2/32"
+
 	if securityGroupFlags.SecurityGroupFile == "" {
 		logrus.Fatalf("请指定要读取安全组规则的 excel 文件。")
 	}
@@ -28,53 +37,29 @@ func securityGroupRuleCreate(cmd *cobra.Command, args []string) {
 		logrus.Fatalf("获取安全组 ID 失败，原因: %v", err)
 	}
 
-	// logrus.Debugf("安全组ID为 %v", sgID)
+	logrus.Debugf("安全组ID为 %v", sgID)
 
-	// body, err := fileparse.GetSecurityGroupRules(securityGroupFlags.SecurityGroupFile, securityGroupFlags.SecurityGroupName)
-	// if err != nil {
-	// 	logrus.Fatalf("获取待创建的安全组规则失败，原因: %v", err)
-	// }
-
-	// for _, rule := range body {
-	// 	response, err := vpcClient.CreateSecurityRule(&model.CreateSecurityRuleRequest{
-	// 		CreateSecurityRuleBody: &model.CreateSecurityRuleBody{
-	// 			SecurityGroupId: &sgID,
-	// 			RemoteType:      rule.RemoteType,
-	// 			Protocol:        rule.Protocol,
-	// 			EtherType:       rule.EtherType,
-	// 			Description:     &rule.Description,
-	// 			RemoteIpPrefix:  &rule.RemoteIpPrefix,
-	// 			Direction:       rule.Direction,
-	// 		},
-	// 	})
-	// 	if err != nil {
-	// 		logrus.Errorf("创建规则失败，原因: %v", err)
-	// 	}
-
-	// 	logrus.Infoln(response)
-	// }
-
-	remoteType := "cidr"
-	protocol := "ANY"
-	etherType := "IPv4"
-	description := "测试描述"
-	direction := "ingress"
-	remoteIpPrefix := "1.1.1.2/32"
-
-	response, err := vpcClient.CreateSecurityRule(&model.CreateSecurityRuleRequest{
-		CreateSecurityRuleBody: &model.CreateSecurityRuleBody{
-			SecurityGroupId: &sgID,
-			RemoteType:      (*model.CreateSecurityRuleBodyRemoteTypeEnum)(&remoteType),
-			Protocol:        (*model.CreateSecurityRuleBodyProtocolEnum)(&protocol),
-			EtherType:       (*model.CreateSecurityRuleBodyEtherTypeEnum)(&etherType),
-			Description:     &description,
-			RemoteIpPrefix:  &remoteIpPrefix,
-			Direction:       (*model.CreateSecurityRuleBodyDirectionEnum)(&direction),
-		},
-	})
+	body, err := fileparse.GetSecurityGroupRules(securityGroupFlags.SecurityGroupFile, securityGroupFlags.SecurityGroupName)
 	if err != nil {
-		logrus.Errorf("创建规则失败，原因: %v", err)
+		logrus.Fatalf("获取待创建的安全组规则失败，原因: %v", err)
 	}
 
-	logrus.Infoln(response)
+	for _, rule := range body {
+		response, err := vpcClient.CreateSecurityRule(&model.CreateSecurityRuleRequest{
+			CreateSecurityRuleBody: &model.CreateSecurityRuleBody{
+				SecurityGroupId: &sgID,
+				RemoteType:      rule.RemoteType,
+				Protocol:        rule.Protocol,
+				EtherType:       rule.EtherType,
+				Description:     &rule.Description,
+				RemoteIpPrefix:  &rule.RemoteIpPrefix,
+				Direction:       rule.Direction,
+			},
+		})
+		if err != nil {
+			logrus.Errorf("创建规则失败，原因: %v", err)
+		}
+
+		logrus.Infoln(response)
+	}
 }
