c649b74bdf657f353790e2ad8c83631c4d932aa2
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2023-07-11 17:35:54 +0800

    Update security_group_rule_create.go

--- cmd/vpc/security_group_rule_create.go
+++ cmd/vpc/security_group_rule_create.go
@@ -1,7 +1,6 @@
 package vpc
 
 import (
-	"github.com/DesistDaydream/ecloud-openapi/pkg/fileparse"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"gitlab.ecloud.com/ecloud/ecloudsdkvpc/model"
@@ -29,48 +28,53 @@ func securityGroupRuleCreate(cmd *cobra.Command, args []string) {
 		logrus.Fatalf("获取安全组 ID 失败，原因: %v", err)
 	}
 
-	logrus.Debugf("安全组ID为 %v", sgID)
+	// logrus.Debugf("安全组ID为 %v", sgID)
 
-	body, err := fileparse.GetSecurityGroupRules(securityGroupFlags.SecurityGroupFile, securityGroupFlags.SecurityGroupName)
-	if err != nil {
-		logrus.Fatalf("获取待创建的安全组规则失败，原因: %v", err)
-	}
-
-	for _, rule := range body {
-		response, err := vpcClient.CreateSecurityRule(&model.CreateSecurityRuleRequest{
-			CreateSecurityRuleBody: &model.CreateSecurityRuleBody{
-				SecurityGroupId: &sgID,
-				RemoteType:      rule.RemoteType,
-				Protocol:        rule.Protocol,
-				EtherType:       rule.EtherType,
-				Description:     &rule.Description,
-				RemoteIpPrefix:  &rule.RemoteIpPrefix,
-				Direction:       rule.Direction,
-			},
-		})
-		if err != nil {
-			logrus.Errorf("创建规则失败，原因: %v", err)
-		}
-
-		logrus.Infoln(response)
-	}
-
-	// remoteType := "cidr"
-	// protocol := "ANY"
-	// etherType := "IPv4"
-	// description := "测试描述"
-	// direction := "ingress"
-	// remoteIpPrefix := "1.1.1.2/32"
-
-	// request := &model.CreateSecurityRuleRequest{
-	// 	CreateSecurityRuleBody: &model.CreateSecurityRuleBody{
-	// 		SecurityGroupId: &sgID,
-	// 		RemoteType:      (*model.CreateSecurityRuleBodyRemoteTypeEnum)(&remoteType),
-	// 		Protocol:        (*model.CreateSecurityRuleBodyProtocolEnum)(&protocol),
-	// 		EtherType:       (*model.CreateSecurityRuleBodyEtherTypeEnum)(&etherType),
-	// 		Description:     &description,
-	// 		RemoteIpPrefix:  &remoteIpPrefix,
-	// 		Direction:       (*model.CreateSecurityRuleBodyDirectionEnum)(&direction),
-	// 	},
+	// body, err := fileparse.GetSecurityGroupRules(securityGroupFlags.SecurityGroupFile, securityGroupFlags.SecurityGroupName)
+	// if err != nil {
+	// 	logrus.Fatalf("获取待创建的安全组规则失败，原因: %v", err)
 	// }
+
+	// for _, rule := range body {
+	// 	response, err := vpcClient.CreateSecurityRule(&model.CreateSecurityRuleRequest{
+	// 		CreateSecurityRuleBody: &model.CreateSecurityRuleBody{
+	// 			SecurityGroupId: &sgID,
+	// 			RemoteType:      rule.RemoteType,
+	// 			Protocol:        rule.Protocol,
+	// 			EtherType:       rule.EtherType,
+	// 			Description:     &rule.Description,
+	// 			RemoteIpPrefix:  &rule.RemoteIpPrefix,
+	// 			Direction:       rule.Direction,
+	// 		},
+	// 	})
+	// 	if err != nil {
+	// 		logrus.Errorf("创建规则失败，原因: %v", err)
+	// 	}
+
+	// 	logrus.Infoln(response)
+	// }
+
+	remoteType := "cidr"
+	protocol := "ANY"
+	etherType := "IPv4"
+	description := "测试描述"
+	direction := "ingress"
+	remoteIpPrefix := "1.1.1.2/32"
+
+	response, err := vpcClient.CreateSecurityRule(&model.CreateSecurityRuleRequest{
+		CreateSecurityRuleBody: &model.CreateSecurityRuleBody{
+			SecurityGroupId: &sgID,
+			RemoteType:      (*model.CreateSecurityRuleBodyRemoteTypeEnum)(&remoteType),
+			Protocol:        (*model.CreateSecurityRuleBodyProtocolEnum)(&protocol),
+			EtherType:       (*model.CreateSecurityRuleBodyEtherTypeEnum)(&etherType),
+			Description:     &description,
+			RemoteIpPrefix:  &remoteIpPrefix,
+			Direction:       (*model.CreateSecurityRuleBodyDirectionEnum)(&direction),
+		},
+	})
+	if err != nil {
+		logrus.Errorf("创建规则失败，原因: %v", err)
+	}
+
+	logrus.Infoln(response)
 }
